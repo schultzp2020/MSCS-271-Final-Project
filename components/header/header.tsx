cd9b342c68d7b0a2fcb833474cebfb5ed3660716
Paul Schultz <pschultz@pobox.com>
2022-04-15 09:38:32 -0500

    initial commit
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

---
+++ components/header/header.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface HeaderProps {}
+
+export const Header = (props: HeaderProps): JSX.Element => {
+  // eslint-disable-next-line no-empty-pattern
+  const {} = props;
+
+  return <div>Hello World</div>;
+};
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Header } from '../components';
+
+const App = (): JSX.Element => (
+  <div className="h-screen w-screen bg-slate-200">
+    <Header />
+  </div>
+);
+
+export default App;
