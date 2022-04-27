d21508b00a68f7f7f3b205d6409d0c439564ef9f
YoungJoel <joeleyoung01@gmail.com>
2022-04-27 11:42:50 -0500

    styled page

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,1 +1,7 @@
-export const Header = (): JSX.Element => <div>The Game of Life</div>;
+export const Header = (): JSX.Element => (
+  <div className="mb-5 flex bg-green-500 px-8 py-6">
+    <div className="flex-auto flex-col text-2xl">The Game Of Life</div>
+    <div className="flex-grow flex-col" />
+    <div className="flex flex-col text-base">By Joel Young and Paul Schultz</div>
+  </div>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        25: 'repeat(25, minmax(0, 1fr))',
+        50: 'repeat(50, minmax(0, 1fr))',
+        100: 'repeat(100, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [],
+  safelist: ['grid-cols-10', 'grid-cols-25', 'grid-cols-50', 'grid-cols-100'],
 };
