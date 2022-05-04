90a8c3e685ab266070a5ab76fb84018abf9ac283
Paul Schultz <pschultz@pobox.com>
2022-05-04 12:37:43 -0500

    completed the conway's game of life
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- components/grid/grid.tsx
+++ components/grid/grid.tsx
@@ -1,17 +1,17 @@
 import type { FormEvent } from 'react';
 import { useState, useCallback } from 'react';
 import { useGameOfLife, useInterval } from '../../hooks';
+import type { Boundary } from '../../hooks';
 
-type GridSizes = 10 | 25 | 50 | 100;
+type GridSizes = 10 | 25 | 50;
 
 export const Grid = (): JSX.Element => {
   const [play, setPlay] = useState<boolean>(false);
   const [timer, setTimer] = useState<number>(100);
   const [showGrid, setShowGrid] = useState<boolean>(false);
-  const [gridSize] = useState<GridSizes>(25);
-  const [gameOfLifeState, gameOfLifeDispatch] = useGameOfLife(gridSize, gridSize);
+  const [gameOfLifeState, gameOfLifeDispatch] = useGameOfLife(10, 10, 'hard');
 
-  const getGridStyle = useCallback(() => {
+  const getGridStyle = useCallback((gridSize) => {
     switch (gridSize) {
       case 10:
         return 'grid-cols-10';
@@ -19,12 +19,10 @@ export const Grid = (): JSX.Element => {
         return 'grid-cols-25';
       case 50:
         return 'grid-cols-50';
-      case 100:
-        return 'grid-cols-100';
       default:
-        return 'grid-cols-25';
+        return 'grid-cols-10';
     }
-  }, [gridSize]);
+  }, []);
 
   useInterval(() => {
     if (play) {
@@ -42,23 +40,46 @@ export const Grid = (): JSX.Element => {
     setShowGrid(target.checked);
   }, []);
 
-  const handleDropdownUpdate = useCallback((e: FormEvent<EventTarget>) => {
-    const target = e.target as HTMLInputElement;
-    console.log(target);
-  }, []);
+  const handleGridSizeDropdownUpdate = useCallback(
+    (e: FormEvent<EventTarget>) => {
+      const target = e.target as HTMLInputElement;
+      const gridSize = Number.parseInt(target.value, 10) as GridSizes;
+      gameOfLifeDispatch({
+        type: 'updateGridSize',
+        payload: { numOfRows: gridSize, numOfColumns: gridSize },
+      });
+    },
+    [gameOfLifeDispatch],
+  );
+
+  const handleBoundaryDropdownUpdate = useCallback(
+    (e: FormEvent<EventTarget>) => {
+      const target = e.target as HTMLInputElement;
+      gameOfLifeDispatch({
+        type: 'updateGridBoundary',
+        payload: { newBoundary: target.value as Boundary },
+      });
+    },
+    [gameOfLifeDispatch],
+  );
 
   return (
     <>
-      <div className={mergeClassNames('mx-auto grid w-fit', getGridStyle())}>
+      <div
+        className={mergeClassNames('mx-auto grid w-fit', getGridStyle(gameOfLifeState.grid.length))}
+      >
         {gameOfLifeState.grid.map((arr, i) =>
           arr.map((cellState, j) => (
             <button
               className={mergeClassNames(
                 'border-1 h-4 w-4 border-black',
                 cellState === 1 ? 'bg-black' : 'bg-white',
-                showGrid ? 'border-2' : 'border-0',
+                showGrid ? 'border-[0.25px]' : 'border-0',
               )}
               type="button"
+              // There is no possible unique key
+              // eslint-disable-next-line react/no-array-index-key
+              key={`cell ${i}${j}}`}
               aria-label={`cell ${i}${j}}`}
               onClick={(): void =>
                 gameOfLifeDispatch({
@@ -70,50 +91,77 @@ export const Grid = (): JSX.Element => {
           )),
         )}
       </div>
-      <button
-        className="mx-2 rounded border-2 border-black bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-        type="button"
-        onClick={(): void => gameOfLifeDispatch({ type: 'nextGeneration' })}
-      >
-        Next generation
-      </button>
-      <button
-        className={mergeClassNames(
-          'mx-2 rounded border-2 border-black py-2 px-4 font-bold text-white',
-          play ? 'bg-slate-500' : 'bg-blue-500 hover:bg-blue-700',
-        )}
-        type="button"
-        onClick={(): void => setPlay(() => !play)}
-      >
-        {play ? 'Pause' : 'Play'}
-      </button>
-      <label htmlFor="timer" className="rounded p-2">
-        In milliseconds:&nbsp;{' '}
-        <input
-          className="w-20 rounded p-2"
-          type="number"
-          min="10"
-          step="10"
-          id="timer"
-          value={timer}
-          onChange={handleTimerUpdate}
-          disabled={play}
-        />
-      </label>
-      <label htmlFor="showGrid">
-        Show Grid:&nbsp;
-        <input type="checkbox" id="showGrid" onChange={handleCheckboxUpdate} />
-      </label>
+      <div className="mt-2">
+        <button
+          className="mx-2 rounded border-2 border-black bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+          type="button"
+          onClick={(): void => gameOfLifeDispatch({ type: 'nextGeneration' })}
+        >
+          Next generation
+        </button>
+        <button
+          className={mergeClassNames(
+            'mx-2 rounded border-2 border-black py-2 px-4 font-bold text-white',
+            play ? 'bg-slate-500' : 'bg-blue-500 hover:bg-blue-700',
+          )}
+          type="button"
+          onClick={(): void => setPlay(() => !play)}
+        >
+          {play ? 'Pause' : 'Play'}
+        </button>
+        <button
+          className="mx-2 rounded border-2 border-black bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+          type="button"
+          onClick={(): void => gameOfLifeDispatch({ type: 'clearGrid' })}
+        >
+          Clear Grid
+        </button>
+        <label htmlFor="timer" className="rounded p-2">
+          In milliseconds:&nbsp;{' '}
+          <input
+            className="w-20 rounded p-2"
+            type="number"
+            min="10"
+            step="10"
+            id="timer"
+            value={timer}
+            onChange={handleTimerUpdate}
+            disabled={play}
+          />
+        </label>
+        <label htmlFor="showGrid">
+          Show Grid:&nbsp;
+          <input type="checkbox" id="showGrid" onChange={handleCheckboxUpdate} />
+        </label>
 
-      <label htmlFor="gridSelect" className="mx-2">
-        Grid Size:&nbsp;
-        <select id="gridSelect" className="rounded p-2" onChange={handleDropdownUpdate}>
-          <option value="10">10x10</option>
-          <option value="25">25x25</option>
-          <option value="50">50x50</option>
-          <option value="100">100x100</option>
-        </select>
-      </label>
+        <label htmlFor="gridSelect" className="mx-2">
+          Grid Size:&nbsp;
+          <select
+            id="gridSelect"
+            className="rounded p-2"
+            onChange={handleGridSizeDropdownUpdate}
+            disabled={play}
+          >
+            <option value="10">10x10</option>
+            <option value="25">25x25</option>
+            <option value="50">50x50</option>
+          </select>
+        </label>
+        <label htmlFor="boundary" className="mx-2">
+          Boundary:&nbsp;
+          <select
+            id="boundary"
+            className="rounded p-2"
+            onChange={handleBoundaryDropdownUpdate}
+            disabled={play}
+          >
+            <option value="hard">Hard</option>
+            <option value="torus">Torus</option>
+            <option value="kleinBottle">Klein Bottle</option>
+            <option value="projectivePlane">Projective Plane</option>
+          </select>
+        </label>
+      </div>
     </>
   );
 };
--- hooks/use-game-of-life/use-game-of-life.tsx
+++ hooks/use-game-of-life/use-game-of-life.tsx
@@ -1,11 +1,12 @@
 import type { Reducer, Dispatch } from 'react';
-import { useReducer, useMemo } from 'react';
+import { useReducer } from 'react';
+
+export type Boundary = 'hard' | 'torus' | 'kleinBottle' | 'projectivePlane';
 
 export interface GameOfLifeState {
   grid: (0 | 1)[][];
+  boundary: Boundary;
 }
-
-export type GameOfLifeActionKind = 'nextGeneration' | 'updateCell' | 'updateGrid';
 
 interface UpdateCellPayload {
   row: number;
@@ -17,7 +18,14 @@ interface UpdateGridPayload {
   newGrid: (0 | 1)[][];
 }
 
-export type GameOfLifeActionPayload = UpdateCellPayload | UpdateGridPayload;
+interface UpdateGridSizePayload {
+  numOfRows: number;
+  numOfColumns: number;
+}
+
+interface UpdateGridBoundary {
+  newBoundary: Boundary;
+}
 
 interface GameOfLifeActionNextGeneration {
   type: 'nextGeneration';
@@ -34,33 +42,51 @@ interface GameOfLifeActionUpdateGrid {
   payload: UpdateGridPayload;
 }
 
+interface GameOfLifeActionUpdateGridSize {
+  type: 'updateGridSize';
+  payload: UpdateGridSizePayload;
+}
+
+interface GameOfLifeActionClearGrid {
+  type: 'clearGrid';
+  payload?: never;
+}
+
+interface GameOfLifeActionUpdateGridBoundary {
+  type: 'updateGridBoundary';
+  payload: UpdateGridBoundary;
+}
+
 export type GameOfLifeAction =
   | GameOfLifeActionNextGeneration
   | GameOfLifeActionUpdateCell
-  | GameOfLifeActionUpdateGrid;
+  | GameOfLifeActionUpdateGrid
+  | GameOfLifeActionUpdateGridSize
+  | GameOfLifeActionClearGrid
+  | GameOfLifeActionUpdateGridBoundary;
 
 export type UseGameOfLife = [GameOfLifeState, Dispatch<GameOfLifeAction>];
 
-export const useGameOfLife = (numOfRows: number, numOfColumns: number): UseGameOfLife => {
-  const grid = useMemo(() => {
-    const g: (0 | 1)[][] = [];
+export const useGameOfLife = (
+  numOfRows: number,
+  numOfColumns: number,
+  boundary: Boundary,
+): UseGameOfLife => {
+  const grid: (0 | 1)[][] = [];
 
-    for (let i = 0; i < numOfRows; i += 1) {
-      g.push(new Array<0 | 1>(numOfColumns).fill(0));
-    }
-
-    return g;
-  }, [numOfRows, numOfColumns]);
+  for (let i = 0; i < numOfRows; i += 1) {
+    grid.push(new Array<0 | 1>(numOfColumns).fill(0));
+  }
 
   const [gameOfLifeState, gameOfLifeDispatch] = useReducer<
-  Reducer<GameOfLifeState, GameOfLifeAction>
-  >(gameOfLifeReducer, { grid });
+    Reducer<GameOfLifeState, GameOfLifeAction>
+  >(gameOfLifeReducer, { grid, boundary });
 
   return [gameOfLifeState, gameOfLifeDispatch];
 };
 
 const gameOfLifeReducer = (state: GameOfLifeState, action: GameOfLifeAction): GameOfLifeState => {
-  const { grid } = state;
+  const { grid, boundary } = state;
   const { type, payload } = action;
 
   if (type === 'nextGeneration') {
@@ -70,10 +96,97 @@ const gameOfLifeReducer = (state: GameOfLifeState, action: GameOfLifeAction): Ga
       for (let m = 0; m < newGrid[l].length; m += 1) {
         // finds alive neighbors
         let aliveNeighbors = 0;
-        for (let i = -1; i < 2; i += 1) {
-          for (let j = -1; j < 2; j += 1) {
-            if (l + i >= 0 && l + i < newGrid.length && m + j >= 0 && m + j < newGrid[l].length) {
-              aliveNeighbors += grid[l + i][m + j];
+
+        if (boundary === 'hard') {
+          for (let i = -1; i < 2; i += 1) {
+            for (let j = -1; j < 2; j += 1) {
+              if (l + i >= 0 && l + i < newGrid.length && m + j >= 0 && m + j < newGrid[l].length) {
+                // normal interior case
+                aliveNeighbors += grid[l + i][m + j];
+              }
+            }
+          }
+        }
+
+        if (boundary === 'torus') {
+          for (let i = -1; i < 2; i += 1) {
+            for (let j = -1; j < 2; j += 1) {
+              // normal interior case but loop horizontal and vertical case
+              aliveNeighbors += grid[mod(l + i, newGrid.length)][mod(m + j, newGrid[l].length)];
+            }
+          }
+        }
+
+        if (boundary === 'kleinBottle') {
+          for (let i = -1; i < 2; i += 1) {
+            for (let j = -1; j < 2; j += 1) {
+              if (l + i >= 0 && l + i < newGrid.length) {
+                // normal interior case
+                aliveNeighbors += grid[l + i][mod(m + j, newGrid[l].length)];
+              } else {
+                // loop horizontal case and swap vertical case
+                aliveNeighbors +=
+                  grid[mod(l + i, newGrid.length)][
+                    mod(newGrid[l].length - m + j - 1, newGrid[l].length)
+                  ];
+              }
+            }
+          }
+        }
+
+        if (boundary === 'projectivePlane') {
+          for (let i = -1; i < 2; i += 1) {
+            for (let j = -1; j < 2; j += 1) {
+              if (
+                (l === 0 || l === newGrid.length - 1) &&
+                (m === 0 || m === newGrid[l].length - 1)
+              ) {
+                // corner case
+                if (i === -1 && j === -1) {
+                  // map to itself
+                  aliveNeighbors += grid[l][m];
+                } else if ((i === -1 && j === 0) || (i === 0 && j === -1)) {
+                  // map to opposite corner
+                  aliveNeighbors +=
+                    grid[l === 0 ? newGrid.length - 1 : 0][m === 0 ? newGrid[l].length - 1 : 0];
+                } else if (i === 1 && j === -1) {
+                  // map to opposite corner adjacent row
+                  aliveNeighbors +=
+                    grid[l === 0 ? newGrid.length - 2 : 1][m === 0 ? newGrid[l].length - 1 : 0];
+                } else if (i === -1 && j === 1) {
+                  // map to opposite corner adjacent column
+                  aliveNeighbors +=
+                    grid[l === 0 ? newGrid.length - 1 : 0][m === 0 ? newGrid[l].length - 2 : 1];
+                }
+              } else if (
+                l + i >= 0 &&
+                l + i < newGrid.length &&
+                m + j >= 0 &&
+                m + j < newGrid[l].length
+              ) {
+                // normal interior case
+                aliveNeighbors += grid[l + i][m + j];
+              } else if (
+                l + i >= 0 &&
+                l + i < newGrid.length &&
+                !(m + j >= 0 && m + j < newGrid[l].length)
+              ) {
+                // swap horizontal case
+                aliveNeighbors +=
+                  grid[mod(newGrid.length - l + i - 1, newGrid.length)][
+                    mod(m + j, newGrid[l].length)
+                  ];
+              } else if (
+                !(l + i >= 0 && l + i < newGrid.length) &&
+                m + j >= 0 &&
+                m + j < newGrid[l].length
+              ) {
+                // swap vertical case
+                aliveNeighbors +=
+                  grid[mod(l + i, newGrid.length)][
+                    mod(newGrid[l].length - m + j - 1, newGrid[l].length)
+                  ];
+              }
             }
           }
         }
@@ -98,21 +211,58 @@ const gameOfLifeReducer = (state: GameOfLifeState, action: GameOfLifeAction): Ga
       }
     }
 
-    return { grid: newGrid };
+    return { grid: newGrid, boundary };
   }
 
   if (type === 'updateCell') {
     const { row, column, cellState } = payload;
     grid[row][column] = cellState;
 
-    return { grid };
+    return { grid, boundary };
   }
 
   if (type === 'updateGrid') {
     const { newGrid } = payload;
 
-    return { grid: newGrid };
+    return { grid: newGrid, boundary };
+  }
+
+  if (type === 'updateGridSize') {
+    const { numOfRows, numOfColumns } = payload;
+
+    const newGrid: (0 | 1)[][] = [];
+
+    for (let i = 0; i < numOfRows; i += 1) {
+      newGrid.push(new Array<0 | 1>(numOfColumns).fill(0));
+    }
+
+    for (let i = 0; i < numOfRows; i += 1) {
+      for (let j = 0; j < numOfColumns; j += 1) {
+        if (i < grid.length && j < grid[i].length) {
+          newGrid[i][j] = grid[i][j];
+        }
+      }
+    }
+
+    return { grid: newGrid, boundary };
+  }
+
+  if (type === 'clearGrid') {
+    const newGrid: (0 | 1)[][] = grid.map((arr) => arr.map(() => 0));
+
+    return { grid: newGrid, boundary };
+  }
+
+  if (type === 'updateGridBoundary') {
+    const { newBoundary } = payload;
+
+    return { grid, boundary: newBoundary };
   }
 
   return state;
 };
+
+// Javascript allows % to return negative remainders
+function mod(n: number, m: number): number {
+  return ((n % m) + m) % m;
+}
